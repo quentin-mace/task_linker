1ad3d07bcce19a468718dc14f07fce4e100180d5
Quentin Macé <154054112+quentin-mace@users.noreply.github.com>
2025-03-10 17:39:26 +0100

    Feat : add fixtures (#3)
    Co-authored-by: Quentin Macé <quentin.mace@quarksgroup.com>

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,65 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Employee;
+use App\Entity\Role;
+use App\Factory\TagFactory;
+use App\Factory\TaskFactory;
+use App\Repository\RoleRepository;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private readonly RoleRepository $roleRepository, private readonly UserPasswordHasherInterface $passwordHasher)
+    {
+    }
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->generateRoles($manager);
+        $this->generateEmployees($manager);
+        TaskFactory::createMany(5);
+        TagFactory::createMany(5);
+    }
+
+    private function generateRoles(ObjectManager $manager): void
+    {
+        $admin = new Role();
+        $admin->setName('admin');
+        $manager->persist($admin);
+
+        $user = new Role();
+        $user->setName('user');
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+
+    private function generateEmployees(ObjectManager $manager): void
+    {
+        $admin = new Employee();
+        $admin->setFirstName('admin');
+        $admin->setLastName('admin');
+        $admin->setEmail('admin@test.io');
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'admin'));
+        $admin->setRole($this->roleRepository->findOneBy(['name' => 'admin']));
+        $admin->setIsActive(true);
+        $admin->setContract('CDI');
+        $admin->setHiringDate(new DateTimeImmutable('2020-01-01'));
+        $manager->persist($admin);
+
+        $user = new Employee();
+        $user->setFirstName('user');
+        $user->setLastName('user');
+        $user->setEmail('user@test.io');
+        $user->setPassword($this->passwordHasher->hashPassword($admin, 'admin'));
+        $user->setRole($this->roleRepository->findOneBy(['name' => 'user']));
+        $user->setIsActive(true);
+        $user->setContract('CDD');
+        $user->setHiringDate(new DateTimeImmutable('2025-01-01'));
+        $manager->persist($user);
 
         $manager->flush();
     }
