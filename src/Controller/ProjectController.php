eed281a382db78c5cd6638bb57ad035084a478d1
Quentin Macé <quentin.mace@quarksgroup.com>
2025-03-13 16:37:20 +0100

    Feat : Finished crud

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -3,8 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Project;
+use App\Form\ProjectType;
 use App\Repository\ProjectRepository;
+use App\Repository\TaskRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -13,10 +18,19 @@ final class ProjectController extends AbstractController
 {
     #[Route('/', name: '')]
     #[Route('/projets', name: '_index')]
-    #[Route('/{$id}/delete', name: '_delete')]
-    public function index(ProjectRepository $repository): Response
+    #[Route('/{id}/delete', name: '_delete')]
+    public function index(?Project $project, ProjectRepository $projectRepository, TaskRepository $taskRepository, EntityManagerInterface $entityManager): Response
     {
-        $projects = $repository->findAll();
+        if ($project) {
+            $entityManager->remove($project);
+            $tasks = $taskRepository->findBy(['project' => $project]);
+            foreach ($tasks as $task) {
+                $entityManager->remove($task);
+            }
+            $entityManager->flush();
+        }
+
+        $projects = $projectRepository->findAll();
 
         return $this->render('projects/index.html.twig', [
             'projects' => $projects,
@@ -31,10 +45,30 @@ final class ProjectController extends AbstractController
         ]);
     }
 
-    #[Route('/{$id}/edit', name: '_edit')]
-    #[Route('/{$id}/create', name: '_create')]
-    public function edit(): Response
+    #[Route('/{id}/edit', name: '_edit', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    #[Route('/create', name: '_create')]
+    public function edit(?Project $project, Request $request, EntityManagerInterface $entityManager): Response
     {
 
+        $project ??= new Project();
+        $form = $this->createForm(ProjectType::class, $project);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $project
+                ->setStartDate(new DateTimeImmutable())
+                ->setIsArchived(false);
+
+            $entityManager->persist($project);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_home_show', ['id' => $project->getId()]);
+        }
+
+        return $this->render('projects/edit.html.twig', [
+            'form' => $form,
+            'action' => $project->getId() ? 'edit' : 'create',
+            'project' => $project,
+        ]);
     }
 }
