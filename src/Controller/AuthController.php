37a75e302a9b230cebfc8549401e6eeed1487848
Quentin Macé <quentin.mace.1110@gmail.com>
2025-05-15 17:39:25 +0200

    Add registration feature with user form and validation
    Implement user registration functionality, including the `RegistrationController`, `RegistrationFormType`, and associated templates. Update the `Employee` entity with default values and unique email constraints. Adjust styles and templates for improved user experience, and refine security.yaml access control.

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,9 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Employee;
+use App\Entity\Role;
+use App\Form\RegistrationFormType;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/auth', name: 'app_dispatch')]
 final class AuthController extends AbstractController
@@ -24,9 +32,23 @@ final class AuthController extends AbstractController
     }
 
     #[Route('/register', name: '_register')]
-    public function register(): Response
+    public function register(?Employee $employee, Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $hasher, ValidatorInterface $validator): Response
     {
-        return $this->render('auth/index.html.twig', [
+        $employee ??= new Employee();
+        $form = $this->createForm(RegistrationFormType::class, $employee);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $employee->setPassword($hasher->hashPassword($employee, $form->get('plain_password')->getData()));
+
+            $entityManager->persist($employee);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('auth/register.html.twig', [
+            'form' => $form,
         ]);
     }
 }
